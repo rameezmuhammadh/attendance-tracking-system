058bade3827150d55237b4725ccd3800a66e17c2
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2025-05-15 02:46:43 +0530

    refactor: simplify AppSidebar by removing unused footer navigation items and consolidating main navigation

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -13,19 +13,15 @@ const mainNavItems: NavItem[] = [
         href: '/dashboard',
         icon: LayoutGrid,
     },
+        {
+        title: 'Subjects',
+        href: '/subjects',
+        icon: BookOpen,
+    },
 ];
 
 const footerNavItems: NavItem[] = [
-    {
-        title: 'Repository',
-        href: 'https://github.com/laravel/react-starter-kit',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#react',
-        icon: BookOpen,
-    },
+
 ];
 
 export function AppSidebar() {

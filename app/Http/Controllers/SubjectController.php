e34c078141b1af4721ecc9221fef3d8f736d53b3
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2025-05-15 02:45:04 +0530

    feat: implement search and pagination in Subject index method

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -2,17 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\SubjectResource;
 use App\Models\Subject;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class SubjectController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $query = Subject::query()->latest();
+
+        // Search by subject name or code
+        if ($search = $request->input('search')) {
+            $terms = explode(' ', $search);
+
+            $query->where(function ($q) use ($terms) {
+                foreach ($terms as $term) {
+                    $q->where(function ($subQuery) use ($term) {
+                        $subQuery->where('name', 'like', '%' . $term . '%')
+                            ->orWhere('code', 'like', '%' . $term . '%');
+                    });
+                }
+            });
+        }
+
+        // Get pagination parameters
+        $perPage = $request->input('per_page', 10);
+        $currentPage = $request->input('page', 1);
+
+        $subjects = $query->paginate($perPage)
+            ->withQueryString();
+
+        return Inertia::render('subjects/index', [
+            'subjects' => SubjectResource::collection($subjects),
+            'params' => array_merge($request->all(), ['page' => $currentPage, 'per_page' => $perPage]),
+        ]);
     }
 
     /**

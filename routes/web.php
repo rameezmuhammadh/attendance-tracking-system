a8bc8c98fbafce13a284b42c339dcfda2bbe2154
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2025-05-15 12:03:41 +0530

    feat: add user resource route to web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -16,6 +17,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::resource('/subjects', SubjectController::class);
     Route::resource('/departments', DepartmentController::class);
+    Route::resource('/users', UserController::class);
 });
 
 require __DIR__ . '/settings.php';

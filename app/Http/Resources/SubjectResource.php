15c7410895a2927cca85a57c83ee5adec84782f6
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2025-05-16 02:31:32 +0530

    feat: add teachers and students relationships to SubjectResource

--- app/Http/Resources/SubjectResource.php
+++ app/Http/Resources/SubjectResource.php
@@ -5,6 +5,8 @@ namespace App\Http\Resources;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\UserResource;
+use App\Http\Resources\StudentResource;
 
 class SubjectResource extends JsonResource
 {
@@ -21,6 +23,12 @@ class SubjectResource extends JsonResource
             'name' => $this->name,
             'code' => $this->code,
             'description' => $this->description,
+            'teachers' => $this->whenLoaded('teachers', function () {
+                return UserResource::collection($this->teachers);
+            }),
+            'students' => $this->whenLoaded('students', function () {
+                return StudentResource::collection($this->students);
+            }),
             'created_at' => (new Carbon($this->created_at))->format('Y-m-d H:i'),
             'updated_at' => (new Carbon($this->updated_at))->format('Y-m-d H:i'),
         ];

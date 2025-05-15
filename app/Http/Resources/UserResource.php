6e0435052ac74de8643383b262776789411865c3
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2025-05-15 12:03:00 +0530

    feat: customize UserResource to transform user data into a structured array

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
 {
+    public static $wrap = false;
     /**
      * Transform the resource into an array.
      *
@@ -14,6 +16,18 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'role' => $this->role,
+            'department_id' => $this->department_id,
+            'department' => $this->whenLoaded('department', function () {
+                return new DepartmentResource($this->department);
+            }),
+            'email_verified_at' => $this->email_verified_at ? (new Carbon($this->email_verified_at))->format('Y-m-d H:i') : null,
+            'created_at' => (new Carbon($this->created_at))->format('Y-m-d H:i'),
+            'updated_at' => (new Carbon($this->updated_at))->format('Y-m-d H:i'),
+        ];
     }
 }

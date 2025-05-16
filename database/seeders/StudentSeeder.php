4b9376a61e2b155dcf7c2ecc20b53bc2bfafdd7e
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2025-05-16 10:07:17 +0530

    feat: update StudentSeeder to generate students with random departments and groups

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -2,10 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Department;
 use App\Models\Student;
 use App\Models\StudentGroup;
+use App\Models\Subject;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class StudentSeeder extends Seeder
 {
@@ -14,13 +17,41 @@ class StudentSeeder extends Seeder
      */
     public function run(): void
     {
-        // Create 10-20 students for each group
-        StudentGroup::all()->each(function ($group) {
-            $numberOfStudents = rand(10, 20);
-            Student::factory($numberOfStudents)->create([
-                'student_group_id' => $group->id,
-                'department_id' => $group->department_id
+        $departments = Department::all();
+        $studentGroups = StudentGroup::all();
+        $subjects = Subject::all();
+
+        // Generate 30 students
+        for ($i = 1; $i <= 30; $i++) {
+            $isFirstYear = $i <= 25; // First 25 students are first year
+            $department = $departments->random();
+            $studentGroup = $studentGroups->random();
+
+            $firstName = fake()->firstName();
+            $lastName = fake()->lastName();
+
+            $student = Student::create([
+                'registration_number' => 'STU' . str_pad($i, 4, '0', STR_PAD_LEFT),
+                'first_name' => $firstName,
+                'last_name' => $lastName,
+                'email' => strtolower($firstName . '.' . $lastName . '@student.example.com'),
+                'is_first_year' => $isFirstYear,
+                'department_id' => $department->id,
+                'student_group_id' => $studentGroup->id,
             ]);
-        });
+
+            // For first year students, assign 3-5 random subjects
+            if ($isFirstYear) {
+                $numSubjects = rand(3, 5);
+                $randomSubjects = $subjects->random($numSubjects);
+
+                $pivotData = [];
+                foreach ($randomSubjects as $subject) {
+                    $pivotData[$subject->id] = ['enrollment_date' => now()->format('Y-m-d')];
+                }
+
+                $student->subjects()->attach($pivotData);
+            }
+        }
     }
 }

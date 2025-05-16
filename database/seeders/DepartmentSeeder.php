5a386e097c817163e10ee0184256bf08fecfb9ad
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2025-05-16 10:07:07 +0530

    feat: update DepartmentSeeder to create specific department records instead of using factory

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -13,6 +13,23 @@ class DepartmentSeeder extends Seeder
      */
     public function run(): void
     {
-        Department::factory(10)->create();
+        $departments = [
+            ['name' => 'Computer Science', 'code' => 'CS', 'description' => 'Department of Computer Science'],
+            ['name' => 'Information Technology', 'code' => 'IT', 'description' => 'Department of Information Technology'],
+            ['name' => 'Software Engineering', 'code' => 'SE', 'description' => 'Department of Software Engineering'],
+            ['name' => 'Data Science', 'code' => 'DS', 'description' => 'Department of Data Science'],
+            ['name' => 'Computer Engineering', 'code' => 'CE', 'description' => 'Department of Computer Engineering'],
+            ['name' => 'Information Systems', 'code' => 'IS', 'description' => 'Department of Information Systems'],
+            ['name' => 'Cybersecurity', 'code' => 'CYB', 'description' => 'Department of Cybersecurity'],
+            ['name' => 'Artificial Intelligence', 'code' => 'AI', 'description' => 'Department of Artificial Intelligence'],
+            ['name' => 'Network Engineering', 'code' => 'NE', 'description' => 'Department of Network Engineering'],
+            ['name' => 'Web Development', 'code' => 'WD', 'description' => 'Department of Web Development'],
+            ['name' => 'Mobile Computing', 'code' => 'MC', 'description' => 'Department of Mobile Computing'],
+            ['name' => 'Cloud Computing', 'code' => 'CC', 'description' => 'Department of Cloud Computing'],
+        ];
+
+        foreach ($departments as $department) {
+            Department::create($department);
+        }
     }
 }
